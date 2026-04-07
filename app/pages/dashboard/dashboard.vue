57e5c18adcc2f381d98aaccda95b4f860d222f60
Delory Rodolphe <roro_spike@hotmail.com>
2026-04-07 08:01:42 +0200

    feat: ajuster la mise en page du tableau de bord pour une meilleure réactivité et accessibilité

--- app/pages/dashboard/dashboard.vue
+++ app/pages/dashboard/dashboard.vue
@@ -13,16 +13,15 @@ onMounted(() => orgStore.fetchMemberships())
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-50 dark:bg-black p-4 lg:p-8">
-    <div class="max-w-[1440px] mx-auto grid grid-cols-12 gap-8">
-      <DashboardAside class="col-span-12 lg:col-span-2" />
+  <div class="min-h-screen bg-gray-50 dark:bg-black p-4 lg:p-8 overflow-x-hidden">
+    <div class="max-w-[1440px] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-4 lg:gap-8">
+      <DashboardAside class="col-span-1 lg:col-span-2" />
 
       <DashboardFlux
-        class="col-span-12 lg:col-span-7"
+        class="col-span-1 lg:col-span-7 w-full min-w-0"
         @select-ticket="showTicket"
       />
-
-      <DashboardOrgs class="col-span-12 lg:col-span-3" />
+      <DashboardOrgs class="col-span-1 lg:col-span-3" />
     </div>
 
     <TicketModal
